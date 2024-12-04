afda8a8aae24b55524dfbc5ae998005b11a49497
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-04 12:13:17 +0200

    add firebase setup to project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:my_store/firebase_options.dart';
 import 'package:my_store/my_store.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyStore());
 }
