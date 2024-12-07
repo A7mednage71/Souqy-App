b806e0caf38ee542dbded01598e3bde011c34632
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-07 23:21:07 +0200

    create static function to getFontFamily based on current app language in fontFamilyHelper

--- lib/core/style/fonts/font_family_helper.dart
+++ lib/core/style/fonts/font_family_helper.dart
@@ -1,4 +1,13 @@
 class FontFamilyHelper {
   static const String arabicCairo = 'Cairo';
   static const String englihsPoppins = 'Poppins';
+
+  static String getFontFamily() {
+    const currentLanguage = 'en';
+    if (currentLanguage == 'en') {
+      return englihsPoppins;
+    } else {
+      return arabicCairo;
+    }
+  }
 }
