871bf85afd83fa6605f8772a1a526de000e4c3b3
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-09 00:39:35 +0200

    add transaltion to lang toogle

--- lib/features/auth/widgets/theme_language_toggle.dart
+++ lib/features/auth/widgets/theme_language_toggle.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_store/core/common/widgets/custom_linear_button.dart';
+import 'package:my_store/core/extensions/localization_context.dart';
 import 'package:my_store/core/extensions/theme_context.dart';
 import 'package:my_store/core/languages/localization_keys.dart';
 import 'package:my_store/core/style/fonts/font_weight_helper.dart';
@@ -25,7 +26,7 @@ class AppThemeAndLanguageToggle extends StatelessWidget {
         CustomLinearButton(
           width: 100.w,
           child: Text(
-            LocalizationKeys.language,
+            context.translate(LocalizationKeys.language),
             style: context.textStyle.copyWith(
               fontSize: 16.sp,
               fontWeight: FontWeightHelper.bold,
