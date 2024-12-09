506f8beb4fb0d471dfd45900dd7581ff5ca47f4b
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-09 02:27:24 +0200

    refactor login form

--- lib/features/auth/login/presentation/views/widgets/login_form.dart
+++ lib/features/auth/login/presentation/views/widgets/login_form.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:my_store/core/animations/animate_do.dart';
-import 'package:my_store/core/common/widgets/custom_text_field.dart';
-import 'package:my_store/core/extensions/localization_context.dart';
-import 'package:my_store/core/helpers/my_validator.dart';
-import 'package:my_store/core/languages/localization_keys.dart';
+import 'package:my_store/features/auth/login/presentation/views/widgets/email_text_form_field.dart';
+import 'package:my_store/features/auth/login/presentation/views/widgets/password_text_form_field.dart';
 
 class LoginForm extends StatefulWidget {
   const LoginForm({super.key});
@@ -16,7 +13,6 @@ class LoginForm extends StatefulWidget {
 class _LoginFormState extends State<LoginForm> {
   AutovalidateMode autovalidateMode = AutovalidateMode.always;
   GlobalKey<FormState> formKey = GlobalKey();
-  bool _isObscure = true;
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -24,47 +20,11 @@ class _LoginFormState extends State<LoginForm> {
       autovalidateMode: autovalidateMode,
       child: Column(
         children: [
-          CustomFadeInRight(
-            duration: 200,
-            child: CustomTextField(
-              controller: TextEditingController(),
-              hintText: LocalizationKeys.email,
-              keyboardType: TextInputType.emailAddress,
-              validator: (value) {
-                if (!MyValidator.isEmailValid(value!)) {
-                  return context.translate(LocalizationKeys.validEmail);
-                }
-                return null;
-              },
-            ),
-          ),
+          const EmailTextFormField(),
           SizedBox(
             height: 20.h,
           ),
-          CustomFadeInRight(
-            duration: 200,
-            child: CustomTextField(
-              controller: TextEditingController(),
-              hintText: LocalizationKeys.password,
-              keyboardType: TextInputType.visiblePassword,
-              obscureText: true,
-              suffixIcon: GestureDetector(
-                child:
-                    Icon(_isObscure ? Icons.visibility_off : Icons.visibility),
-                onTap: () {
-                  setState(() {
-                    _isObscure = !_isObscure;
-                  });
-                },
-              ),
-              validator: (value) {
-                if (!MyValidator.isEmailValid(value!)) {
-                  return context.translate(LocalizationKeys.validPasswrod);
-                }
-                return null;
-              },
-            ),
-          ),
+          const PasswordTextFormField(),
         ],
       ),
     );
