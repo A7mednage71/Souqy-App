3344ca39f59c6c64f814452b5cec7aed4d6479f4
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-09 03:56:36 +0200

    add signup routes and navigate to signup screen from CreateAccountButton

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -3,6 +3,7 @@ import 'package:my_store/core/common/screens/no_route_screen.dart';
 import 'package:my_store/core/routes/base_material_page_route.dart';
 import 'package:my_store/core/routes/routes.dart';
 import 'package:my_store/features/auth/login/presentation/views/login_screen.dart';
+import 'package:my_store/features/auth/register/presentation/views/sighn_up_screen.dart';
 import 'package:my_store/home_screen.dart';
 
 class AppRouter {
@@ -13,6 +14,8 @@ class AppRouter {
         return BaseRoute(page: const HomeScreen());
       case Routes.login:
         return BaseRoute(page: const LoginScreen());
+      case Routes.sighnUp:
+        return BaseRoute(page: const SighnUpScreen());
       default:
         return BaseRoute(page: const NoRouteScreen());
     }
--- lib/features/auth/login/presentation/views/widgets/create_account_button.dart
+++ lib/features/auth/login/presentation/views/widgets/create_account_button.dart
@@ -2,8 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_store/core/animations/animate_do.dart';
 import 'package:my_store/core/extensions/localization_context.dart';
+import 'package:my_store/core/extensions/navigation_context.dart';
 import 'package:my_store/core/extensions/theme_context.dart';
 import 'package:my_store/core/languages/localization_keys.dart';
+import 'package:my_store/core/routes/routes.dart';
 import 'package:my_store/core/style/fonts/font_weight_helper.dart';
 
 class CreateAccountButton extends StatelessWidget {
@@ -16,7 +18,9 @@ class CreateAccountButton extends StatelessWidget {
     return CustomFadeInDown(
       duration: 400,
       child: TextButton(
-        onPressed: () {},
+        onPressed: () {
+          context.pushReplacementNamed(Routes.sighnUp);
+        },
         child: Text(
           context.translate(LocalizationKeys.createAccount),
           style: context.textStyle.copyWith(
