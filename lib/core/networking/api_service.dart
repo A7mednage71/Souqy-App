e778950bde7c60fe71f5755f428f348bd18354c9
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-10 21:18:34 +0200

    add login and userRole to api service

--- lib/core/networking/api_service.dart
+++ lib/core/networking/api_service.dart
@@ -8,4 +8,12 @@ part 'api_service.g.dart';
 @RestApi(baseUrl: ApiConstants.baseUrl)
 abstract class ApiService {
   factory ApiService(Dio dio, {String baseUrl}) = _ApiService;
+
+  @POST(ApiConstants.graphQl)
+  Future<void> login(
+    @Body() Map<String, dynamic> body,
+  );
+
+  @GET(ApiConstants.userPorfile)
+  Future<void> userRole();
 }
