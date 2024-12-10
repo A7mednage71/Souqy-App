5849ef96fa7bbc74f7653ef10c66c6bbfbecb400
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-10 22:46:29 +0200

    add userRole to login repo

--- lib/features/auth/login/data/repos/login_repo.dart
+++ lib/features/auth/login/data/repos/login_repo.dart
@@ -5,12 +5,14 @@ import 'package:my_store/core/networking/api_service.dart';
 import 'package:my_store/core/networking/graph_queries/auth/login_query.dart';
 import 'package:my_store/features/auth/login/data/models/login_request_model.dart';
 import 'package:my_store/features/auth/login/data/models/login_response_model.dart';
+import 'package:my_store/features/auth/login/data/models/user_role_model.dart';
 
 class LoginRepo {
   LoginRepo(this._apiService);
 
   final ApiService _apiService;
 
+  // Login
   Future<ApiResult<LoginResponseModel>> login({
     required LoginRequestModel loginRequest,
   }) async {
@@ -24,5 +26,11 @@ class LoginRepo {
       }
       return ApiResult.failure(ServerFailure(e.toString()));
     }
+  }
+
+  // User Role
+  Future<UserRoleModel> userRole() async {
+    final result = await _apiService.userRole();
+    return result;
   }
 }
