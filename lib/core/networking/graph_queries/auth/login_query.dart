9309eec5e1e41318329fe729624c6d79829b219f
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-10 21:38:11 +0200

    create graph login query

---
+++ lib/core/networking/graph_queries/auth/login_query.dart
@@ -0,0 +1,24 @@
+import 'package:my_store/features/auth/login/data/models/login_request_model.dart';
+
+class LoginQuery {
+  LoginQuery._();
+
+  static final LoginQuery instance = LoginQuery._();
+
+  Map<String, dynamic> loginQuery({required LoginRequestModel model}) {
+    return {
+      'query': r'''
+        mutation Login($email: String!, $password: String!) {
+          login(email: $email, password: $password) {
+            access_token
+            refresh_token
+          }
+        }
+      ''',
+      'variables': {
+        'email': model.email,
+        'password': model.password,
+      },
+    };
+  }
+}
