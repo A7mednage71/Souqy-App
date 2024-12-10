4880d48ec5f729abf7e5a6382f778c8e81c3049d
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-10 22:06:16 +0200

    create login repo

---
+++ lib/features/auth/login/data/repos/login_repo.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+import 'package:my_store/core/networking/api_error.dart';
+import 'package:my_store/core/networking/api_result.dart';
+import 'package:my_store/core/networking/api_service.dart';
+import 'package:my_store/core/networking/graph_queries/auth/login_query.dart';
+import 'package:my_store/features/auth/login/data/models/login_request_model.dart';
+
+class LoginRepo {
+  LoginRepo(this._apiService);
+
+  final ApiService _apiService;
+
+  Future<ApiResult<void>> login({
+    required LoginRequestModel loginRequest,
+  }) async {
+    try {
+      final result = await _apiService
+          .login(LoginQuery.instance.loginQuery(model: loginRequest));
+      return ApiResult.success(result);
+    } catch (e) {
+      if (e is DioException) {
+        return ApiResult.failure(ServerFailure.fromDioError(e));
+      }
+      return ApiResult.failure(ServerFailure(e.toString()));
+    }
+  }
+}
