bdc1b4924e38440314fe872b2028f6e86e3df2ae
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-11 02:34:45 +0200

    - set di for login bloc - add bloc provider for login bloc in app router

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_store/core/common/screens/no_route_screen.dart';
+import 'package:my_store/core/di/dependency_injection.dart';
 import 'package:my_store/core/routes/base_material_page_route.dart';
 import 'package:my_store/core/routes/routes.dart';
+import 'package:my_store/features/auth/login/data/repos/login_repo.dart';
+import 'package:my_store/features/auth/login/presentation/bloc/login_bloc.dart';
 import 'package:my_store/features/auth/login/presentation/views/login_screen.dart';
 import 'package:my_store/features/auth/register/presentation/views/sighn_up_screen.dart';
 import 'package:my_store/home_screen.dart';
@@ -13,7 +17,12 @@ class AppRouter {
       case Routes.home:
         return BaseRoute(page: const HomeScreen());
       case Routes.login:
-        return BaseRoute(page: const LoginScreen());
+        return BaseRoute(
+          page: BlocProvider(
+            create: (context) => LoginBloc(getIt<LoginRepo>()),
+            child: const LoginScreen(),
+          ),
+        );
       case Routes.sighnUp:
         return BaseRoute(page: const SighnUpScreen());
       default:
