adc636b93b7b72d34b50d32867c15cbbaf899663
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-14 21:21:19 +0200

    create dashboard items

--- lib/features/admin/dashboard/presentation/dashboard_screen.dart
+++ lib/features/admin/dashboard/presentation/dashboard_screen.dart
@@ -1,15 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_store/core/common/widgets/custom_admin_appbar.dart';
+import 'package:my_store/core/style/images/app_images.dart';
+import 'package:my_store/features/admin/dashboard/presentation/widgets/dashboard_item.dart';
 
 class DashboardScreen extends StatelessWidget {
   const DashboardScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAdminAppBar(title: 'Dashboard'),
-      body: Center(
-        child: Text('Dashboard Screen'),
+    return Scaffold(
+      appBar: const CustomAdminAppBar(title: 'Dashboard'),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20),
+        child: RefreshIndicator(
+          onRefresh: () async {},
+          child: Column(
+            children: [
+              const DashboardItem(
+                title: 'Products',
+                count: '50',
+                image: AppImages.imagesAdminProductsDrawer,
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              const DashboardItem(
+                title: 'Categories',
+                count: '50',
+                image: AppImages.imagesAdminCategoriesDrawer,
+              ),
+              SizedBox(
+                height: 20.h,
+              ),
+              const DashboardItem(
+                title: 'Users',
+                count: '50',
+                image: AppImages.imagesAdminUsersDrawer,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
