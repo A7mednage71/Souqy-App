36dab6b05d84fe11f2f9c6d3c96669cc4ddb1c06
A7med_Nage7 <a.nageh@bayanatz.com>
2024-12-15 04:08:22 +0200

    refreshDahsboardItemsValue

--- lib/features/admin/dashboard/views/presentation/widgets/dashboard_screen_body.dart
+++ lib/features/admin/dashboard/views/presentation/widgets/dashboard_screen_body.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_store/features/admin/dashboard/views/bloc/number_of_categories/fetch_number_of_categories_bloc.dart';
+import 'package:my_store/features/admin/dashboard/views/bloc/number_of_products/fetch_number_of_products_bloc.dart';
+import 'package:my_store/features/admin/dashboard/views/bloc/number_of_users/fetch_number_of_users_bloc.dart';
 import 'package:my_store/features/admin/dashboard/views/presentation/widgets/categories_dash_item.dart';
 import 'package:my_store/features/admin/dashboard/views/presentation/widgets/products_dash_item.dart';
 import 'package:my_store/features/admin/dashboard/views/presentation/widgets/users_dash_item.dart';
@@ -12,7 +16,9 @@ class DashBoardScreenBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return RefreshIndicator(
-      onRefresh: () async {},
+      onRefresh: () async {
+        refreshDahsboardItemsValue(context);
+      },
       child: ListView(
         children: [
           const ProductsDashboarditem(),
@@ -27,5 +33,17 @@ class DashBoardScreenBody extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  void refreshDahsboardItemsValue(BuildContext context) {
+    context
+        .read<FetchNumberOfProductsBloc>()
+        .add(const FetchNumberOfProductsEvent.fetchNumberOfProducts());
+    context
+        .read<FetchNumberOfCategoriesBloc>()
+        .add(const FetchNumberOfCategoriesEvent.fetchNumberOfCategories());
+    context
+        .read<FetchNumberOfUsersBloc>()
+        .add(const FetchNumberOfUsersEvent.fetchNumberOfUsers());
   }
 }
