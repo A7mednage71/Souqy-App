169678b55fa367ff90e27af10400a7c251a1e65f
A7med_Nage7 <a.nageh@bayanatz.com>
2025-01-29 03:00:55 +0200

    add create category repo and bloc logic

--- lib/features/admin/categories/data/repos/categories_repo.dart
+++ lib/features/admin/categories/data/repos/categories_repo.dart
@@ -4,6 +4,8 @@ import 'package:my_store/core/networking/api_result.dart';
 import 'package:my_store/core/networking/api_service.dart';
 import 'package:my_store/core/networking/graph_queries/admin/categories/categories_guery.dart';
 import 'package:my_store/features/admin/categories/data/models/categories_response_model.dart';
+import 'package:my_store/features/admin/categories/data/models/create_category_request_model.dart';
+import 'package:my_store/features/admin/categories/data/models/create_category_response_model.dart';
 
 class CategoriesRepo {
   CategoriesRepo(this._apiService);
@@ -11,7 +13,25 @@ class CategoriesRepo {
   Future<ApiResult<CategoriesResponseModel>> getCategories() async {
     try {
       final response =
-          await _apiService.getCategories(CategoriesGuery.categoriesQuery());
+          await _apiService.getCategories(CategoriesGuery.getCategoriesQuery());
+      return ApiResult.success(response);
+    } catch (e) {
+      if (e is DioException) {
+        return ApiResult.failure(ServerFailure.fromDioError(e));
+      }
+      return ApiResult.failure(ServerFailure(e.toString()));
+    }
+  }
+
+  Future<ApiResult<CreateCategoryResponseModel>> createCategory({
+    required CreateCategoryRequestModel createCategoryModel,
+  }) async {
+    try {
+      final response = await _apiService.createCategory(
+        CategoriesGuery.createCategoryQuery(
+          model: createCategoryModel,
+        ),
+      );
       return ApiResult.success(response);
     } catch (e) {
       if (e is DioException) {
---
+++ lib/features/admin/categories/presentation/bloc/create_category/create_category_bloc.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:my_store/features/admin/categories/data/models/create_category_request_model.dart';
+import 'package:my_store/features/admin/categories/data/models/create_category_response_model.dart';
+import 'package:my_store/features/admin/categories/data/repos/categories_repo.dart';
+
+part 'create_category_bloc.freezed.dart';
+part 'create_category_event.dart';
+part 'create_category_state.dart';
+
+class CreateCategoryBloc
+    extends Bloc<CreateCategoryEvent, CreateCategoryState> {
+  CreateCategoryBloc(this._categoriesRepo)
+      : super(const CreateCategoryState.initial()) {
+    on<CreateCategory>(createCategory);
+  }
+
+  final CategoriesRepo _categoriesRepo;
+
+  TextEditingController categoryNameController = TextEditingController();
+  String categoryImage = '';
+  final formKey = GlobalKey<FormState>();
+
+  FutureOr<void> createCategory(
+    CreateCategory event,
+    Emitter<CreateCategoryState> emit,
+  ) async {
+    emit(const CreateCategoryState.loading());
+    final result = await _categoriesRepo.createCategory(
+      createCategoryModel: CreateCategoryRequestModel(
+        name: categoryNameController.text,
+        image: categoryImage,
+      ),
+    );
+    result.when(
+      success: (response) => emit(CreateCategoryState.success(response)),
+      failure: (error) => emit(CreateCategoryState.failure(error.errMessages)),
+    );
+  }
+}
