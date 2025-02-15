db2a3d3bd7771b8a2c024d3cadb515337f9c0ba9
A7med_Nage7 <a.nageh@bayanatz.com>
2025-02-16 00:23:58 +0200

    create other customer screens

---
+++ lib/features/customer/categories/presentation/views/customer_categories_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CustomerCategoriesScreen extends StatelessWidget {
+  const CustomerCategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Categories'),
+      ),
+    );
+  }
+}
