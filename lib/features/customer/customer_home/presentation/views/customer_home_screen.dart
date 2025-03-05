4d915670e8ceaed3e4449952b7c6111be444bee4
A7med_Nage7 <a.nageh@bayanatz.com>
2025-03-05 02:30:36 +0200

    create home screen banners slider

--- lib/features/customer/customer_home/presentation/views/customer_home_screen.dart
+++ lib/features/customer/customer_home/presentation/views/customer_home_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:my_store/core/animations/animate_do.dart';
+import 'package:my_store/core/extensions/theme_context.dart';
+import 'package:my_store/features/customer/customer_home/presentation/views/widgets/home_screen_banners_slider.dart';
 
 class CustomerHomeScreen extends StatelessWidget {
   const CustomerHomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('Customer Home'));
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
+      child: Stack(
+        children: [
+          const Column(
+            children: [
+              HomeScreenBannersSlider(),
+            ],
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: CustomFadeInLeft(
+              duration: 200,
+              child: FloatingActionButton(
+                backgroundColor: context.themeColors.bluePinkLight,
+                child: const Icon(Icons.arrow_upward, color: Colors.white),
+                onPressed: () {},
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
