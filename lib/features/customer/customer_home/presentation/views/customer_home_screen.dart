7ebb0d57be2a7f28cabd88a6974c7f36a3f2976c
A7med_Nage7 <a.nageh@bayanatz.com>
2025-03-05 03:48:32 +0200

    get  and show customer categories successfully done

--- lib/features/customer/customer_home/presentation/views/customer_home_screen.dart
+++ lib/features/customer/customer_home/presentation/views/customer_home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_store/core/animations/animate_do.dart';
+import 'package:my_store/core/di/dependency_injection.dart';
 import 'package:my_store/core/extensions/theme_context.dart';
+import 'package:my_store/features/customer/customer_home/presentation/bloc/get_customer_categories/get_customer_categories_bloc.dart';
 import 'package:my_store/features/customer/customer_home/presentation/views/widgets/customer_home_categories_list_view.dart';
 import 'package:my_store/features/customer/customer_home/presentation/views/widgets/home_screen_banners_slider.dart';
 
@@ -10,29 +13,33 @@ class CustomerHomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
-      child: Stack(
-        children: [
-          Column(
-            children: [
-              const HomeScreenBannersSlider(),
-              SizedBox(height: 20.h),
-              const CustomerHomeCategoriesListView(),
-            ],
-          ),
-          Align(
-            alignment: Alignment.bottomRight,
-            child: CustomFadeInLeft(
-              duration: 200,
-              child: FloatingActionButton(
-                backgroundColor: context.themeColors.bluePinkLight,
-                child: const Icon(Icons.arrow_upward, color: Colors.white),
-                onPressed: () {},
+    return BlocProvider(
+      create: (context) => getIt<GetCustomerCategoriesBloc>()
+        ..add(const GetCustomerCategoriesEvent.getCategories()),
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
+        child: Stack(
+          children: [
+            Column(
+              children: [
+                const HomeScreenBannersSlider(),
+                SizedBox(height: 20.h),
+                const CustomerHomeCategoriesListView(),
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: CustomFadeInLeft(
+                duration: 200,
+                child: FloatingActionButton(
+                  backgroundColor: context.themeColors.bluePinkLight,
+                  child: const Icon(Icons.arrow_upward, color: Colors.white),
+                  onPressed: () {},
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
--- lib/features/customer/customer_home/presentation/views/widgets/customer_category_item.dart
+++ lib/features/customer/customer_home/presentation/views/widgets/customer_category_item.dart
@@ -3,10 +3,11 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_store/core/common/widgets/show_cached_image.dart';
 import 'package:my_store/core/extensions/theme_context.dart';
 import 'package:my_store/core/style/fonts/font_weight_helper.dart';
+import 'package:my_store/features/admin/categories/data/models/categories_response_model.dart';
 
 class CustomerCategoryItem extends StatelessWidget {
-  const CustomerCategoryItem({super.key});
-
+  const CustomerCategoryItem({required this.category, super.key});
+  final Category category;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -15,13 +16,13 @@ class CustomerCategoryItem extends StatelessWidget {
       child: Column(
         children: [
           ShowCachedNetworkImage(
-            image: 'https://picsum.photos/200/300',
+            image: category.image,
             height: 70.h,
             width: 70.w,
           ),
           SizedBox(height: 5.h),
           Text(
-            'Category name',
+            category.name,
             style: context.textStyle.copyWith(
               fontSize: 12.sp,
               fontWeight: FontWeightHelper.medium,
