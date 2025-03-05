8a18b7e52771b89a93c30985f250254ab584a8bb
A7med_Nage7 <a.nageh@bayanatz.com>
2025-03-05 05:49:34 +0200

    refresh categories with RefreshIndicator

--- lib/features/customer/customer_home/presentation/views/customer_home_screen.dart
+++ lib/features/customer/customer_home/presentation/views/customer_home_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:my_store/core/animations/animate_do.dart';
-import 'package:my_store/core/di/dependency_injection.dart';
 import 'package:my_store/core/extensions/theme_context.dart';
 import 'package:my_store/features/customer/customer_home/presentation/bloc/get_customer_categories/get_customer_categories_bloc.dart';
 import 'package:my_store/features/customer/customer_home/presentation/views/widgets/customer_home_categories_list_view.dart';
@@ -34,50 +33,50 @@ class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => getIt<GetCustomerCategoriesBloc>()
-        ..add(const GetCustomerCategoriesEvent.getCategories()),
-      child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
-        child: Stack(
-          children: [
-            RefreshIndicator(
-              onRefresh: () async {},
-              child: SingleChildScrollView(
-                controller: scrollController,
-                child: Column(
-                  children: [
-                    const HomeScreenBannersSlider(),
-                    SizedBox(height: 20.h),
-                    const CustomerHomeCategoriesListView(),
-                    SizedBox(height: 20.h),
-                    const CustomerHomeProductsGridView(),
-                    SizedBox(height: 20.h),
-                    const ViewAllProductsButton(),
-                    SizedBox(height: 50.h),
-                  ],
-                ),
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
+      child: Stack(
+        children: [
+          RefreshIndicator(
+            onRefresh: () async {
+              context.read<GetCustomerCategoriesBloc>().add(
+                    const GetCustomerCategoriesEvent.getCategories(),
+                  );
+            },
+            child: SingleChildScrollView(
+              controller: scrollController,
+              child: Column(
+                children: [
+                  const HomeScreenBannersSlider(),
+                  SizedBox(height: 20.h),
+                  const CustomerHomeCategoriesListView(),
+                  SizedBox(height: 20.h),
+                  const CustomerHomeProductsGridView(),
+                  SizedBox(height: 20.h),
+                  const ViewAllProductsButton(),
+                  SizedBox(height: 50.h),
+                ],
               ),
             ),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: CustomFadeInLeft(
-                duration: 200,
-                child: FloatingActionButton(
-                  backgroundColor: context.themeColors.bluePinkLight,
-                  child: const Icon(Icons.arrow_upward, color: Colors.white),
-                  onPressed: () {
-                    scrollController.animateTo(
-                      0,
-                      duration: const Duration(milliseconds: 500),
-                      curve: Curves.fastOutSlowIn,
-                    );
-                  },
-                ),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: CustomFadeInLeft(
+              duration: 200,
+              child: FloatingActionButton(
+                backgroundColor: context.themeColors.bluePinkLight,
+                child: const Icon(Icons.arrow_upward, color: Colors.white),
+                onPressed: () {
+                  scrollController.animateTo(
+                    0,
+                    duration: const Duration(milliseconds: 500),
+                    curve: Curves.fastOutSlowIn,
+                  );
+                },
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
--- lib/features/customer/customer_main/presentation/views/widgets/get_main_screen_body_bloc_builder.dart
+++ lib/features/customer/customer_main/presentation/views/widgets/get_main_screen_body_bloc_builder.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:my_store/core/di/dependency_injection.dart';
 import 'package:my_store/features/customer/categories/presentation/views/customer_categories_screen.dart';
+import 'package:my_store/features/customer/customer_home/presentation/bloc/get_customer_categories/get_customer_categories_bloc.dart';
 import 'package:my_store/features/customer/customer_home/presentation/views/customer_home_screen.dart';
 import 'package:my_store/features/customer/customer_main/presentation/cubit/bottom_navigation_cubit.dart';
 import 'package:my_store/features/customer/favorites/presentation/views/favorites_screen.dart';
@@ -17,7 +19,11 @@ class GetMainScreenBodyBlocBuilder extends StatelessWidget {
       child: BlocBuilder<BottomNavigationCubit, BottomNavigationState>(
         builder: (context, state) {
           return state.when(
-            home: () => const CustomerHomeScreen(),
+            home: () => BlocProvider(
+              create: (context) => getIt<GetCustomerCategoriesBloc>()
+                ..add(const GetCustomerCategoriesEvent.getCategories()),
+              child: const CustomerHomeScreen(),
+            ),
             categories: () => const CustomerCategoriesScreen(),
             favorites: () => const FavoritesScreen(),
             profile: () => const ProfileScreen(),
