faa87db6b4beb8bc44a8ee3d413397f63b0b5348
A7med_Nage7 <a.nageh@bayanatz.com>
2025-03-23 16:03:36 +0200

    add notification to firebase from admin

--- lib/features/admin/notifications/data/repos/notifications_repo.dart
+++ lib/features/admin/notifications/data/repos/notifications_repo.dart
@@ -1,8 +1,12 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dio/dio.dart';
 import 'package:my_store/core/networking/api_error.dart';
 import 'package:my_store/core/networking/api_result.dart';
 import 'package:my_store/core/services/cloud_messaging/firebase_messaging_helper.dart';
 import 'package:my_store/features/admin/notifications/data/models/notification_model.dart';
+import 'package:my_store/features/customer/notifications/data/models/customer_notification_model.dart';
 
 class NotificationsRepo {
   Future<ApiResult<void>> sendNotification({
@@ -21,6 +25,27 @@ class NotificationsRepo {
         return ApiResult.failure(ServerFailure.fromDioError(e));
       }
       return ApiResult.failure(ServerFailure(e.toString()));
+    }
+  }
+
+  // add a notification to all users in firebase
+  Future<void> addNotificationToFirebase({
+    required CustomerNotificationModel model,
+  }) async {
+    try {
+      final firestore = FirebaseFirestore.instance;
+
+      final users = await firestore.collection('users').get();
+
+      for (final user in users.docs) {
+        await firestore
+            .collection('users')
+            .doc(user.id)
+            .collection('notifications')
+            .add(model.toJson());
+      }
+    } catch (e) {
+      log('Firebase error : $e');
     }
   }
 }
--- lib/features/admin/notifications/presentation/bloc/send_notification/send_notification_bloc.dart
+++ lib/features/admin/notifications/presentation/bloc/send_notification/send_notification_bloc.dart
@@ -2,8 +2,11 @@ import 'dart:async';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:my_store/core/extensions/date_extension.dart';
 import 'package:my_store/features/admin/notifications/data/models/notification_model.dart';
 import 'package:my_store/features/admin/notifications/data/repos/notifications_repo.dart';
+import 'package:my_store/features/customer/notifications/data/models/customer_notification_model.dart';
+import 'package:uuid/uuid.dart';
 
 part 'send_notification_bloc.freezed.dart';
 part 'send_notification_event.dart';
@@ -26,6 +29,16 @@ class SendNotificationBloc
     try {
       await _notificationsRepo.sendNotification(
         model: event.model,
+      );
+      await _notificationsRepo.addNotificationToFirebase(
+        model: CustomerNotificationModel(
+          notificationId: const Uuid().v4(),
+          title: event.model.title,
+          body: event.model.body,
+          productId: event.model.productId.toString(),
+          createdAt: event.model.createdAt.convertDataFormate(),
+          isSeen: false,
+        ),
       );
       emit(const SendNotificationState.success());
     } catch (e) {
